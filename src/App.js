7a8fa6062a5e2dc0b21ed6a0e5dc621b7e580e4a
reactdevelopmentlpcc@gmail.com <jeffrey.graham600@gmail.com>
2023-01-10 22:18:16 -0500

    faq

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 import Services from "./components/Services/Services";
 import About from "./components/About/About";
@@ -15,6 +16,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
