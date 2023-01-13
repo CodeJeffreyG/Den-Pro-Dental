a722e10b8a2ea88c31157f574c5fc8731a0d925c
reactdevelopmentlpcc@gmail.com <jeffrey.graham600@gmail.com>
2023-01-13 16:48:58 -0500

    cant make mui declaritive :(

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import aboutData from "./aboutData";
 import AboutTitle from "./AboutTitle";
-import AboutValues from "./AboutValues";
+import AnchorIcon from "@mui/icons-material/Anchor";
+import HexagonIcon from "@mui/icons-material/Hexagon";
 
 import "./about.scss";
 
@@ -11,12 +12,150 @@ const About = () => {
   return (
     <div className="about-container">
       <AboutTitle />
-      <AboutValues />
-      <AboutValues />
-      <AboutValues />
-      <AboutValues />
-      <AboutValues />
-      <AboutValues />
+      <div className="value-container">
+        <div className="value-icon-container">
+          <HexagonIcon
+            sx={{
+              fontSize: 120,
+              color: "#FFFFFF",
+              transform: "rotate(30deg)",
+            }}
+          ></HexagonIcon>
+          <AnchorIcon
+            sx={{ color: "black", position: "absolute", fontSize: "60px" }}
+          />
+        </div>
+        <main>
+          <h2>title</h2>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Consequatur voluptatem laudantium, labore ea eveniet perspiciatis
+            veniam repudiandae ex aperiam dolores! Vero quisquam ut officiis
+            dolore facilis quos fuga, excepturi itaque!
+          </p>
+        </main>
+      </div>
+      <div className="value-container">
+        <div className="value-icon-container">
+          <HexagonIcon
+            sx={{
+              fontSize: 120,
+              color: "#FFFFFF",
+              transform: "rotate(30deg)",
+            }}
+          ></HexagonIcon>
+          <AnchorIcon
+            sx={{ color: "black", position: "absolute", fontSize: "60px" }}
+          />
+        </div>
+        <main>
+          <h2>title</h2>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Consequatur voluptatem laudantium, labore ea eveniet perspiciatis
+            veniam repudiandae ex aperiam dolores! Vero quisquam ut officiis
+            dolore facilis quos fuga, excepturi itaque!
+          </p>
+        </main>
+      </div>
+      <div className="value-container">
+        <div className="value-icon-container">
+          <HexagonIcon
+            sx={{
+              fontSize: 120,
+              color: "#FFFFFF",
+              transform: "rotate(30deg)",
+            }}
+          ></HexagonIcon>
+          <AnchorIcon
+            sx={{ color: "black", position: "absolute", fontSize: "60px" }}
+          />
+        </div>
+        <main>
+          <h2>title</h2>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Consequatur voluptatem laudantium, labore ea eveniet perspiciatis
+            veniam repudiandae ex aperiam dolores! Vero quisquam ut officiis
+            dolore facilis quos fuga, excepturi itaque!
+          </p>
+        </main>
+      </div>
+      <div className="value-container">
+        <div className="value-icon-container">
+          <HexagonIcon
+            sx={{
+              fontSize: 120,
+              color: "#FFFFFF",
+              transform: "rotate(30deg)",
+            }}
+          ></HexagonIcon>
+          <AnchorIcon
+            sx={{ color: "black", position: "absolute", fontSize: "60px" }}
+          />
+        </div>
+        <main>
+          <h2>title</h2>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Consequatur voluptatem laudantium, labore ea eveniet perspiciatis
+            veniam repudiandae ex aperiam dolores! Vero quisquam ut officiis
+            dolore facilis quos fuga, excepturi itaque!
+          </p>
+        </main>
+      </div>
+      <div className="value-container">
+        <div className="value-icon-container">
+          <HexagonIcon
+            sx={{
+              fontSize: 120,
+              color: "#FFFFFF",
+              transform: "rotate(30deg)",
+            }}
+          ></HexagonIcon>
+          <AnchorIcon
+            sx={{ color: "black", position: "absolute", fontSize: "60px" }}
+          />
+        </div>
+        <main>
+          <h2>title</h2>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Consequatur voluptatem laudantium, labore ea eveniet perspiciatis
+            veniam repudiandae ex aperiam dolores! Vero quisquam ut officiis
+            dolore facilis quos fuga, excepturi itaque!
+          </p>
+        </main>
+      </div>
+      <div className="value-container">
+        <div className="value-icon-container">
+          <HexagonIcon
+            sx={{
+              fontSize: 120,
+              color: "#FFFFFF",
+              transform: "rotate(30deg)",
+            }}
+          ></HexagonIcon>
+          <AnchorIcon
+            sx={{ color: "black", position: "absolute", fontSize: "60px" }}
+          />
+        </div>
+        <main>
+          <h2>title</h2>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Consequatur voluptatem laudantium, labore ea eveniet perspiciatis
+            veniam repudiandae ex aperiam dolores! Vero quisquam ut officiis
+            dolore facilis quos fuga, excepturi itaque!
+          </p>
+        </main>
+      </div>
     </div>
   );
 };
--- src/components/About/aboutData.js
+++ src/components/About/aboutData.js
@@ -1,9 +1,36 @@
-import firstImage from "../public-static/firstpic.webp";
-import secondImage from "../public-static/secondpic.webp";
-import thirdImage from "../public-static/thirdpic.webp";
+import AnchorIcon from "@mui/icons-material/Anchor";
 
 const aboutData = [
-  
+  {
+    icon: "AnchorIcon",
+    title: "title",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quia,enim possimus blanditiis nihil expedita esse perferendis liberodolore. Praesentium et quos aliquid, qui nulla veniam delectus aliasrecusandae ullam.",
+  },
+  {
+    icon: AnchorIcon,
+    title: "title",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quia,enim possimus blanditiis nihil expedita esse perferendis liberodolore. Praesentium et quos aliquid, qui nulla veniam delectus aliasrecusandae ullam.",
+  },
+  {
+    icon: AnchorIcon,
+    title: "title",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quia,enim possimus blanditiis nihil expedita esse perferendis liberodolore. Praesentium et quos aliquid, qui nulla veniam delectus aliasrecusandae ullam.",
+  },
+  {
+    icon: AnchorIcon,
+    title: "title",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quia,enim possimus blanditiis nihil expedita esse perferendis liberodolore. Praesentium et quos aliquid, qui nulla veniam delectus aliasrecusandae ullam.",
+  },
+  {
+    icon: AnchorIcon,
+    title: "title",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quia,enim possimus blanditiis nihil expedita esse perferendis liberodolore. Praesentium et quos aliquid, qui nulla veniam delectus aliasrecusandae ullam.",
+  },
+  {
+    icon: AnchorIcon,
+    title: "title",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quia,enim possimus blanditiis nihil expedita esse perferendis liberodolore. Praesentium et quos aliquid, qui nulla veniam delectus aliasrecusandae ullam.",
+  },
 ];
 
 export default aboutData;
