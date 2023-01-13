9c7f7b7569691a6351a862f51e0b92528f1b7049
reactdevelopmentlpcc@gmail.com <jeffrey.graham600@gmail.com>
2023-01-13 15:30:47 -0500

    styling icon

--- src/components/About/AboutValues.jsx
+++ src/components/About/AboutValues.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import HexagonIcon from "@mui/icons-material/Hexagon";
+import AnchorIcon from "@mui/icons-material/Anchor";
 import "./about.scss";
+import { color } from "@mui/system";
 const AboutValues = () => {
   return (
     <div className="value-container">
       <div className="value-icon-container">
         <HexagonIcon
-          sx={{ fontSize: 120, color: "#FFFFFF", transform: "rotate(30deg)" }}
+          sx={{
+            fontSize: 120,
+            color: "#FFFFFF",
+            transform: "rotate(30deg)",
+          }}
         ></HexagonIcon>
+        <AnchorIcon
+          sx={{ color: "black", position: "absolute", fontSize: "60px" }}
+        />
       </div>
       <main>
         <h2>title</h2>
